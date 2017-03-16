742d58d49e67ed3311726b0d843623e03f1a433c
Nico <nicolas.esprit.bdl@outlook.com>
2017-03-16 16:58:25 +0100

    Bypass Exception for running UnitTests with ReactiveUI on Xamarin Studio for Mac

--- src/ReactiveUI/PortableLibraryStubs.cs
+++ src/ReactiveUI/PortableLibraryStubs.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace ReactiveUI
 {
-    public class PlatformRegistrations : IWantsToRegisterStuff
-    {
-        public void Register(Action<Func<object>, Type> registerFunction)
-        {
-            throw new Exception("You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
-        }
-    }
+	public class PlatformRegistrations : IWantsToRegisterStuff
+	{
+		public void Register(Action<Func<object>, Type> registerFunction)
+		{
+			if (Splat.ModeDetector.InUnitTestRunner())
+			{
+				System.Diagnostics.Debug.WriteLine("UnitTestRunner, bypass Exception : You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
+				return;
+			}
+
+			throw new Exception("You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
+		}
+	}
 }
